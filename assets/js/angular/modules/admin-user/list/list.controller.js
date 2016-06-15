95670be0a0945913e653bbde1c6e55720a066d31
Batiga Faouzane <fb@enyosolutions.com>
2016-06-15 18:37:35 +0200

    feat: admin edit user

--- assets/js/angular/modules/admin-user/list/list.controller.js
+++ assets/js/angular/modules/admin-user/list/list.controller.js
@@ -1,11 +1,20 @@
 'use strict';
 
-angular.module('start.controllers').controller("AdminUserListCtrl", function($scope, $location, Auth, AdminUser, UserService) {
+angular.module('start.controllers').controller("AdminUserListCtrl", function($scope, $location, $filter, Auth, AdminUser, UserService) {
    $scope.users = AdminUser.query();
 
     $scope.activateUser = function(index){
         $scope.users[index].isActive = true;
         $scope.users[index].$update();
+    };
+    $scope.deleteUser = function(index){
+        console.log(index);
+        $scope.users[index].$delete();
+        $scope.users.splice(index, 1);
+    };
+
+    $scope.editUser = function(index){
+        $scope.users[index].isActive = true;
     };
 
 
