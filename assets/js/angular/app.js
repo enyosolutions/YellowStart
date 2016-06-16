7dc3b251880323673399564bb138895480325a47
Manu Ohana <manu.ohana@hurgent.com>
2016-06-16 18:09:33 +0200

    loading screen

--- assets/js/angular/app.js
+++ assets/js/angular/app.js
@@ -64,5 +64,28 @@ angular
     })
     .config(['$httpProvider', function ($httpProvider) {
         $httpProvider.interceptors.push('AuthInterceptor');
-    }]);
+    }])
 
+    .directive('loadingScreen', ['$timeout', '$animate', function($timeout, $animate) {
+        return({
+            link: link,
+            restrict: "C"
+        });
+
+        function link(scope, element, attributes) {
+            $animate.leave(element.children().eq(1)).then(
+                function cleanupAfterAnimation() {
+                    $timeout(function () {
+                        scope.startFade = true;
+
+                        $timeout(function () {
+                            element.remove();
+
+                            scope.unlocked = true;
+                            scope = element = attributes = null;
+                        }, 500);
+                    }, 1000);
+                }
+            );
+        }
+    }]);
