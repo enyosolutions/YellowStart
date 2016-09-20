9d9494390a4c976be6d7ba1a38d19c4a7cf56d2c
Batiga Faouzane <fb@enyosolutions.com>
2016-09-20 17:55:11 +0200

    fix: tag administration

--- assets/js/angular/modules/admin-tag/list/list.controller.js
+++ assets/js/angular/modules/admin-tag/list/list.controller.js
@@ -5,13 +5,13 @@ angular.module('start.controllers').controller("AdminTagListCtrl", function($sco
     $scope.currentPage = 0;
     $scope.tags = Tag.query({page:$scope.currentPage});
     var query = {'sort[name]':1};
+
     $scope.prevPage = function () {
         if ($scope.currentPage > 0) {
             $scope.currentPage--;
         }
         $scope.tags = Tag.query(angular.extend({page: $scope.currentPage}, query));
     };
-
 
     $scope.nextPage = function () {
         $scope.currentPage++;
@@ -20,14 +20,8 @@ angular.module('start.controllers').controller("AdminTagListCtrl", function($sco
 
     $scope.deleteTag = function(index){
         console.log(index);
-        $scope.Tag[index].$delete();
-        $scope.Tag.splice(index, 1);
-    };
-
-
-    $scope.search = function () {
-        $scope.Tag = Tag.query({search: $scope.searchInput});
-        return;
+        $scope.tags[index].$delete();
+        $scope.tags.splice(index, 1);
     };
 
 });
