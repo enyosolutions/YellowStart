6407fc324f8f831cbeaa6c7d9940c58ad5a5937c
Gabriel <gabriel.hofman.00@gmail.com>
2016-10-03 13:37:41 +0200

    Home startup slider responsive update

--- assets/js/angular/modules/startup/list/list.controller.js
+++ assets/js/angular/modules/startup/list/list.controller.js
@@ -214,8 +214,34 @@ angular.module('start.controllers')
                     variableWidth: true,
                     speed: 300,
                     centerMode: false,
-                    slidesToShow: 4,
-                    slidesToScroll: 1
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    responsive: [
+                      {
+                        breakpoint: 1300,
+                        settings: {
+                          slidesToShow: 4
+                        }
+                      },
+                      {
+                        breakpoint: 1024,
+                        settings: {
+                          slidesToShow: 3
+                        }
+                      },
+                      {
+                        breakpoint: 600,
+                        settings: {
+                          slidesToShow: 2
+                        }
+                      },
+                      {
+                        breakpoint: 480,
+                        settings: {
+                          slidesToShow: 1
+                        }
+                      }
+                    ]
                 });
             }
         };
